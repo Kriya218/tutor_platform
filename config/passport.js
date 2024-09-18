9f62d17f6803baa4a441692834f7d09017ccb255
Kriya <kriya101101@gmail.com>
2024-09-18 15:22:47 +0800

    Add passport-google-oauth20

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const GoogleStrategy = require('passport-google-oauth20')
 const bcrypt = require('bcryptjs')
 const { User } = require('../models')
 
@@ -27,6 +28,36 @@ passport.use(new LocalStrategy(
       .catch(err => cb(err))
   }
 ))
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  passReqToCallback: true
+}, (req, accessToken, refreshToken, profile, cb) => {
+  const email = profile.emails[0].value
+  const name = profile.displayName
+  const image = profile.photos[0].value
+  return User.findOne({where: { email }})
+    .then(user => {
+      if (user) {
+        return cb(null, user)
+      }
+      const randomPwd = Math.random().toString(36).slice(-8)
+      return bcrypt.hash(randomPwd, 10)
+        .then(hash => User.create({ 
+          name,
+          email,
+          password: hash,
+          image
+        }))
+        .then(user => cb(null, { id: user.id, name: user.name, email: user.email}))
+        .catch(err => {
+          req.flash('error_msg', '登入失敗')
+          cb(err)
+        })
+      })
+}))
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,23 @@ const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
 
 const { authenticated } = require('../middlewares/auth')
-
 const { generalErrorHandler } = require('../middlewares/error-handler')
+
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', {
-  failureRedirect:'/signin', failureFlash: true }), userController.signIn)
+  failureRedirect:'/signin',
+  failureFlash: true 
+}), userController.signIn)
+router.get('/oauth/login/google', passport.authenticate('google', { scope:['email'] }))
+router.get('/oauth/redirect/google', passport.authenticate('google', {
+  failureRedirect:'/signin',
+  failureFlash: true
+}), userController.signIn)
 router.get('/logout', userController.logout)
+
 router.get('/tutors', authenticated, userController.getTutors)
 
 router.get('/', (req, res) => res.redirect('/tutors'))
